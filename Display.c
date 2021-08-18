c93bf0fd9646d76e14b32c73abd9955698fb2b1b
WenceslaoFB <francobouvetw@fcal.uner.edu.ar>
2021-08-18 17:31:47 -0300

    Firt commit 18/8/2021

---
+++ Display.c
@@ -0,0 +1,19 @@
+/*
+ * Display.c
+ *
+ * Created: 18/8/2021 16:38:43
+ *  Author: Admin
+ */ 
+
+#include "Display.h"
+
+static void fun2();
+
+static int myVar;
+
+void refreshDisplay(){
+	
+}
+
+
+
